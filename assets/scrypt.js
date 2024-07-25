3a9a346fe292c955aea79475d4d5e405d786c068
Darvel <marsonyteam@gmail.com>
2024-07-26 00:54:19 +0200

    Animation for footer

--- assets/scrypt.js
+++ assets/scrypt.js
@@ -182,3 +182,21 @@ var swiper = new Swiper(".slide-content", {
       },
     },
   });
+  gsap.fromTo(".footer",
+    {
+        opacity: 0,
+        y: "50vh", 
+        bottom: "-20vh" 
+    },
+    {
+        opacity: 1,
+        y: "10vh", 
+        bottom: "7vh",
+        scrollTrigger: {
+            trigger: ".containerlog2",
+            start: "top bottom", 
+            end: "top center", 
+            scrub: true
+        }
+    }
+);
