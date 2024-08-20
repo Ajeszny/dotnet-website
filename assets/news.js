095b52adc9c21c577072e45a89b783f0eaf38be4
Darvel <marsonyteam@gmail.com>
2024-08-20 23:56:37 +0200

    News page done for now(whole web page is responsive as i can say;)

--- assets/news.js
+++ assets/news.js
@@ -1,18 +1,21 @@
-gsap.fromTo(".footer",
-    {
-        opacity: 0,
-        y: "30vh", 
-        bottom: "20vh" 
-    },
-    {
-        opacity: 1,
-        y: "8vh", 
-        bottom: "7vh",
-        scrollTrigger: {
-            trigger: ".containerlog2",
-            start: "top bottom", 
-            end: "top center", 
-            scrub: true
-        }
-    }
-);
+
+const btn_news=document.querySelector(".news_news");
+const btn_events=document.querySelector(".news_events");
+const content_news=document.querySelector(".news_news_div");
+const content_events=document.querySelector(".events");
+btn_events.addEventListener("click",()=>{
+    content_events.style.display = "block"; 
+    content_news.style.display = "none"; 
+    btn_events.classList.add("news_active_button");
+    btn_events.classList.remove("news_nonactive_button");
+    btn_news.classList.remove("news_active_button");
+    btn_news.classList.add("news_nonactive_button");
+});
+btn_news.addEventListener("click",()=>{
+    content_events.style.display = "none"; 
+    content_news.style.display = "block"; 
+    btn_news.classList.add("news_active_button");
+    btn_news.classList.remove("news_nonactive_button");
+    btn_events.classList.remove("news_active_button");
+    btn_events.classList.add("news_nonactive_button");
+});
