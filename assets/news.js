e0b3ed615078b73653d8b60c84b5da9a38945803
Darvel <marsonyteam@gmail.com>
2024-09-08 00:35:49 +0200

    Some admin features added and created header+footer.css file, which helped us reduce code in another files

--- assets/news.js
+++ assets/news.js
@@ -4,7 +4,7 @@ const btn_events=document.querySelector(".news_events");
 const content_news=document.querySelector(".news_news_div");
 const content_events=document.querySelector(".events");
 btn_events.addEventListener("click",()=>{
-    content_events.style.display = "block"; 
+    content_events.style.display = "flex"; 
     content_news.style.display = "none"; 
     btn_events.classList.add("news_active_button");
     btn_events.classList.remove("news_nonactive_button");
@@ -13,9 +13,12 @@ btn_events.addEventListener("click",()=>{
 });
 btn_news.addEventListener("click",()=>{
     content_events.style.display = "none"; 
-    content_news.style.display = "block"; 
+    content_news.style.display = "flex"; 
     btn_news.classList.add("news_active_button");
     btn_news.classList.remove("news_nonactive_button");
     btn_events.classList.remove("news_active_button");
     btn_events.classList.add("news_nonactive_button");
 });
+function toggleText(element) {
+    element.classList.toggle('expanded_for_news');
+}
---
+++ assets/project.js
@@ -0,0 +1,18 @@
+gsap.fromTo(".footer",
+    {
+        opacity: 0,
+        y: "30vh", 
+        bottom: "20vh" 
+    },
+    {
+        opacity: 1,
+        y: "9vh", 
+        bottom: "7vh",
+        scrollTrigger: {
+            trigger: ".containerlog2",
+            start: "top bottom", 
+            end: "top center", 
+            scrub: true
+        }
+    }
+);
